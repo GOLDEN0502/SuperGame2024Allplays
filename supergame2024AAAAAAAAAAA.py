6b9a24e7dbf4c08762f3e3675c58186c8096de83
GOLDEN0502 <123xcw@gmail.com>
2024-11-02 12:37:58 +0300

    Update supergame2024AAAAAAAAAAA.py

--- supergame2024AAAAAAAAAAA.py
+++ supergame2024AAAAAAAAAAA.py
@@ -1,36 +1,75 @@
 import os
 import msvcrt
+
+class Field:
+    def __init__(self, size, p):
+        self.size = size
+        self.arr = [[0 for _ in range(size)] for _ in range(size)]
+        self.arr[p.coordX][p.coordY] = 1
+
+    def UpdateField(self, p):
+        self.arr[p.prevX][p.prevY] = 0
+        self.arr[p.coordX][p.coordY] = 1
+
+    def PrintField(self):
+        os.system('cls')
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.arr[j][i] == 0:
+                    print(" ", end="")
+                elif self.arr[j][i] == 1:
+                    print("@", end="")
+            print("|")
+        print("_" * self.size)
+
 class Player:
-    health = 3 
-    coordX = 1
-    coordY = 1
+    def __init__(self, health, coordX, coordY):
+        self.health = health
+        self.coordX = coordX
+        self.coordY = coordY
+        self.prevX = coordX  
+        self.prevY = coordY 
+
     def MoveRight(self):
-        os.system('cls')
+        self.prevX = self.coordX
+        self.prevY = self.coordY
         self.coordX += 1
-        print(self.coordY*'\n' + self.coordX*" " + '@')
+
     def MoveLeft(self):
-        os.system('cls')
+        self.prevX = self.coordX
+        self.prevY = self.coordY
         self.coordX -= 1
-        print(self.coordY*'\n' + self.coordX*" " + '@')
+
     def MoveUp(self):
-        os.system('cls')
+        self.prevX = self.coordX
+        self.prevY = self.coordY
         self.coordY -= 1
-        print(self.coordY*'\n' + self.coordX*" " + '@')
+
     def MoveDown(self):
-        os.system('cls')
+        self.prevX = self.coordX
+        self.prevY = self.coordY
         self.coordY += 1
-        print(self.coordY*'\n' + self.coordX*" " + '@')
 
-P = Player()
-lock = 20
+P = Player(3, 1, 1)
+f = Field(20, P)
+lock = 19
 
-while 1:
+while(1):
+    f.PrintField()
     uinput = msvcrt.getch().decode('ASCII')
-    if(uinput == 'd' and P.coordX < lock  or uinput == 'D' and P.coordX < lock):   
+    
+    if (uinput == 'd' or uinput == 'D') and P.coordX < lock:
         P.MoveRight()
-    if(uinput == 'A' and P.coordX > 0 or uinput == 'a' and P.coordX > 0):    
+        f.UpdateField(P)
+    
+    if (uinput == 'a' or uinput == 'A') and P.coordX > 0:
         P.MoveLeft()
-    if(uinput == 'W' and P.coordY > 0 or uinput == 'w' and P.coordY > 0):
+        f.UpdateField(P)
+        
+    if (uinput == 'w' or uinput == 'W') and P.coordY > 0:
         P.MoveUp()
-    if(uinput == 'S' and P.coordY < lock or uinput == 's' and P.coordY < lock):
+        f.UpdateField(P)
+        
+    if (uinput == 's' or uinput == 'S') and P.coordY < lock:
         P.MoveDown()
+        f.UpdateField(P)
