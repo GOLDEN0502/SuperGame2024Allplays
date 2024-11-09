c9bd826da4398662d6be85a39dd49ff3a0f2869a
GOLDEN0502 <123xcw@gmail.com>
2024-11-09 15:41:13 +0300

    Update supergame2024AAAAAAAAAAA.py

--- supergame2024AAAAAAAAAAA.py
+++ supergame2024AAAAAAAAAAA.py
@@ -1,5 +1,39 @@
 import os
 import msvcrt
+import random
+import sys
+  
+score = 0
+
+
+class Coin:
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+    def UpdateCoin(self):
+        self.x = random.randint(1, 19)
+        self.y = random.randint(1, 19)
+        
+class Enemy:
+    def __init__(self, xe,ye,):
+        self.xe = xe
+        self.ye = ye
+        self.prevxe = xe
+        self.prevye = ye
+        
+    def UpdateEnemy(self):
+        self.prevxe = self.xe
+        self.prevye = self.ye
+        a = random.randint(0, 1)
+        b = random.randint(0, 1)
+        if( a == 0 and b == 0 and self.prevxe != 1):
+            self.xe -= 1
+        if( a == 0 and b == 1 and self.prevxe != 17):
+            self.xe += 1
+        if( a == 1 and b == 0 and self.prevye != 1):
+            self.ye -=1
+        if( a == 1 and b == 1 and self.prevye != 17):
+            self.ye += 1
 
 class Field:
     def __init__(self, size, p):
@@ -8,7 +42,10 @@ class Field:
         self.arr[p.coordX][p.coordY] = 1
 
     def UpdateField(self, p):
+        self.arr[e.prevxe][e.prevye] = 0
+        self.arr[c.x][c.y] = 2
         self.arr[p.prevX][p.prevY] = 0
+        self.arr[e.xe][e.ye] = 3
         self.arr[p.coordX][p.coordY] = 1
 
     def PrintField(self):
@@ -19,10 +56,19 @@ class Field:
                     print(" ", end="")
                 elif self.arr[j][i] == 1:
                     print("@", end="")
+                elif self.arr[j][i] == 2:
+                    print("o", end="")
+                elif self.arr[j][i] == 3:
+                    print("Z", end="")
+                    
             print("|")
         print("_" * self.size)
-
+        print(score)
+        print(P.health)
+            
+        
 class Player:
+    
     def __init__(self, health, coordX, coordY):
         self.health = health
         self.coordX = coordX
@@ -52,15 +98,37 @@ class Player:
 
 P = Player(3, 1, 1)
 f = Field(20, P)
+coinX = random.randint(1, 19)
+coinY = random.randint(1, 19)
+c = Coin(coinX, coinY)
+enemyXE = random.randint(1, 15)
+enemyYE = random.randint(1, 15)
+e = Enemy(enemyYE,enemyXE)
 lock = 19
 
+
 while(1):
+    if (P.coordX == c.x) and (P.coordY == c.y):
+        c.UpdateCoin()
+        score += 1
+    if (P.prevX == e.prevxe) and (P.prevY == e.prevye):
+        P.health -= 1
+
+        if(P.health <= 0) :
+         
+            sys.exit(0)
+        
+    e.UpdateEnemy()
+
+
+        
     f.PrintField()
     uinput = msvcrt.getch().decode('ASCII')
     
     if (uinput == 'd' or uinput == 'D') and P.coordX < lock:
         P.MoveRight()
         f.UpdateField(P)
+        
     
     if (uinput == 'a' or uinput == 'A') and P.coordX > 0:
         P.MoveLeft()
